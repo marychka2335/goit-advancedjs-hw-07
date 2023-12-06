54aa96a3c964b5df57aed200377c719131dd99aa
marychka2335 <marychka2335@icloud.com>
2023-12-06 16:22:18 +0200

    first commit

--- src/1.ts
+++ src/1.ts
@@ -4,16 +4,7 @@
   напишіть скорочену ініціалізацію.
 */
 
-class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+class Student {constructor(private name: string, private age: number, private grade: string) {}
 }
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,63 @@
+
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+    this.door = false; // the door is permanently closed
+  }
+
+  abstract openDoor(key: Key): void; 
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("A person entered the house");
+    } else {
+      console.log("The door is closed");
+    }
+  }
+}
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open. Come in");
+    } else {
+      console.log("Invalid key");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
-export {};
